8facbe6e4b248f5879a47d6de88775cea6d47ae2
alexandrabenamar <alexandra.benamar@yahoo.Fr>
2018-08-24 17:31:36 +0200

    minor modifications

--- src/drive.py
+++ src/drive.py
@@ -10,13 +10,11 @@ from oauth2client import file, client, tools
 from oauth2client.client import OAuth2WebServerFlow
 
 from httplib2 import Http
-import webbrowser
 import io
+import webbrowser
+
 
 def drive_credentials():
-    """
-        Get credentials and build the Google Drive service.
-    """
     SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.metadata']
     CREDENTIAL_PATH = './credentials/credentials.json'
     store = file.Storage('token.json')
@@ -28,39 +26,40 @@ def drive_credentials():
     return drive_service
 
 
-def search_file(FILE_NAME):
+
+def search_file(drive_service, FILE_NAME):
     """
         Search and open a file from Google Drive.
             # FILE_NAME: name of the file to be searched for or keyword contained in it.
     """
-    results = DRIVE.files().list(fields="nextPageToken,files(id, name, webViewLink)").execute()
+    results = drive_service.files().list(fields="nextPageToken, files(id, name, webViewLink)").execute()
     items = results.get('files', [])
 
     if not items:
-        print('No files found.')
+        print('No file named %s was found' %FILE_NAME)
     else:
         for item in items:
-            if (FILE_NAME in item['name']):
-                webbrowser.open(item['webViewLink'])  # Go to item['webViewLink']
+            if (str(FILE_NAME).lower() in str(item['name']).lower()):
+                webbrowser.open(item['webViewLink'])
 
-def upload(FILE_PATH, MIME_TYPE):
+
+def upload_file(drive_service, FILE_PATH, MIME_TYPE):
     """
         Upload a file to Google Drive.
             x FILE_PATH : path of the file to upload
             x MIME_TYPE : output format of the file
                 For more informations : https://github.com/odeke-em/drive/wiki/List-of-MIME-type-short-keys
-
     """
     file_metadata = {'name': 'test.jpeg'}
     media = MediaFileUpload(FILE_PATH, mimetype=MIME_TYPE)
-    file_service = DRIVE.files().create(body=file_metadata,
+    file_service = drive_service.files().create(body=file_metadata,
                     media_body=media,
                     fields='id').execute()
 
     print('File ID: %s' % file_service.get('id'))
 
 
-def download_file(FILE_ID, MIME_TYPE, FILENAME):
+def download_file(drive_service, FILE_ID, MIME_TYPE, FILENAME):
     """
         Download a file from Google Drive.
             x FILE_ID : id of the file on Google Drive
@@ -69,13 +68,13 @@ def download_file(FILE_ID, MIME_TYPE, FILENAME):
     """
     if "google-apps" in MIME_TYPE:                                        #the file has a google format
         if ("document" or "form") in MIME_TYPE:
-            request = DRIVE.files().export_media(fileId=FILE_ID,
+            request = drive_service.files().export_media(fileId=FILE_ID,
                             mimeType="text/plain")
         elif "spreadsheet" in MIME_TYPE:
-            request = DRIVE.files().export_media(fileId=FILE_ID,
+            request = drive_service.files().export_media(fileId=FILE_ID,
                             mimeType="text/csv")
     else:
-        request = DRIVE.files().get_media(fileId=FILE_ID)
+        request = drive_service.files().get_media(fileId=FILE_ID)
     fh = io.FileIO(FILENAME, 'wb')
     downloader = MediaIoBaseDownload(fh, request)
     done = False
@@ -85,5 +84,5 @@ def download_file(FILE_ID, MIME_TYPE, FILENAME):
 
 
 if __name__ == '__main__':
-    DRIVE = drive_credentials()
-    # apply funtion
+    drive_service = drive_credentials()
+    search_file(drive_service, "DocTest")
